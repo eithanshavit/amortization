ce8baa5cbffea0892b7fc1de90dcfa2b116ca063
Eithan Shavit <eithanshavit@gmail.com>
2016-07-07 23:21:02 -0700

    Better validation + tests

--- index.js
+++ index.js
@@ -7,14 +7,40 @@ function ceil2dec(num) {
   return (Math.round(num * 100) / 100.0);
 }
 
+// Validation
+
+function assertNumber(num, message) {
+  if (!(typeof num === "number")) {
+    throw new TypeError(message);
+  }
+}
+
+function assertInt(num, message) {
+  if (!((typeof num === "number") && Math.floor(num) === num)) {
+    throw new TypeError(message);
+  }
+}
+
+function assertPos(num, message) {
+  if ( !((typeof num === "number") && num > 0) ) {
+    throw new RangeError(message);
+  }
+}
+
 // Exports
 
-exports.amortizationSchedule = function(principal, years, rate) {
-  var monthlyPayment = finance.AM(principal, rate, years, 0);
+exports.amortizationSchedule = function(principal, yearsDuration, yearlyRate) {
+  assertNumber(principal, "principal must be a number");
+  assertPos(principal, "principal must be positive");
+  assertNumber(yearlyRate, "yearlyRate must be a number");
+  assertPos(yearlyRate, "yearlyRate must be positive");
+  assertInt(yearsDuration, "yearsDuration must be a and integer");
+  assertPos(yearsDuration, "yearsDuration must be positive");
+  var monthlyPayment = finance.AM(principal, yearlyRate, yearsDuration, 0);
   var amortizationSchedule = [];
-  for (var i = 0; i < (years * 12); i++) {
+  for (var i = 0; i < (yearsDuration * 12); i++) {
     var prevPrincipal = i == 0 ? principal : amortizationSchedule[i-1].principal;
-    var interestPayment = ceil2dec(prevPrincipal * rate/ 12.0 / 100);
+    var interestPayment = ceil2dec(prevPrincipal * yearlyRate/ 12.0 / 100);
     var principalPayment = ceil2dec(monthlyPayment - interestPayment);
     amortizationSchedule.push({
       paymentNumber: i+1,
---
+++ test/test.js
@@ -0,0 +1,45 @@
+var expect = require('chai').expect;
+var amortization = require('../../amortization');
+describe('amortizationSchedule', function() {
+  var fn;
+
+  before(function() {
+   fn = amortization.amortizationSchedule;
+  });
+
+  describe('principal', function() {
+
+    it('should be positive', function() {
+      expect(function() {fn(-1, 1, 1)}).to.throw(RangeError, "principal must be positive");
+    });
+
+    it('should be a number', function() {
+      expect(function() {fn("text", 1, 1)}).to.throw(TypeError, "principal must be a number");
+    });
+
+  });
+
+  describe('yearsDuration', function() {
+
+    it('should be positive', function() {
+      expect(function() {fn(1, -1, 1)}).to.throw(RangeError, "yearsDuration must be positive");
+    });
+
+    it('should be an integer', function() {
+      expect(function() {fn(1, 1.2, 1)}).to.throw(TypeError, "yearsDuration must be a and integer");
+    });
+
+  });
+
+  describe('yearlyRate', function() {
+
+    it('should be positive', function() {
+      expect(function() {fn(1, 1, -1)}).to.throw(RangeError, "yearlyRate must be positive");
+    });
+
+    it('should be a number', function() {
+      expect(function() {fn(1, 1, "text")}).to.throw(TypeError, "yearlyRate must be a number");
+    });
+
+  });
+});
